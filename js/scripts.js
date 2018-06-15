eea2148b77f2b377179aff44b3d9047163933d9a
Guest User <Guest@epicodus-4.local>
2018-06-15 09:35:17 -0700

    Add JS scripts, order button

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//business logic
+function Pizza(name, size, number, toppings) {
+  this.name = name;
+  this.size = size;
+  this.number = number;
+  this.toppings = toppings;
+}
+
+
+
+
+
+//user interface
+$(document).ready(function () {
+  function resetFields() {
+    $("input#name").val("");
+    $("select#size").val("");
+    $("select#number").val("");
+    $("select#toppings").val("");
+  }
+//purchase button
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+
+    var nameinput = $("input#name").val();
+    var sizeinput = $("select#size").val();
+    var numberinput = $("select#number").val();
+    var toppingsinput = $("select#toppings").val();
+
+    var newPizza = new Pizza(nameinput, sizeinput, numberinput, toppingsinput)
+
+    $("h3#ordered").append("<li><span class='pizza'>" + "Thank you, " + newPizza.name + " click here to see your order" + "</span></li>")
+//showorder
+    $(".pizza").last().click(function() {
+      $("#show-order").show();
+      $("#show-order h2").text(newPizza.name);
+      $("#listname").text(newPizza.name);
+      $("#listsize").text(newPizza.size);
+      $("#listnumber").text(newPizza.number);
+      $("#listtoppings").text(newPizza.toppings);
+    });
+    resetFields();
+  });
+
+
+
+
+
+});
