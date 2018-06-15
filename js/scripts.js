8dd17ca2807bee8ced56aab6202a844aca1190c8
Eliot Charette <eliotcharette@gmail.com>
2018-06-15 11:33:33 -0700

    Add prototype to calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,40 @@
 //business logic
-function Pizza(name, size, number, toppings) {
+function Pizza(name, size, number, top1, top2, top3) {
   this.name = name;
   this.size = size;
   this.number = number;
-  this.toppings = [];
-}
-function Toppings(top1, top2, top3, ulimited) {
   this.top1 = top1;
   this.top2 = top2;
   this.top3 = top3;
-  this.unlimited = unlimited;
 }
-
-
-
-
-
+Pizza.prototype.price = function() {
+  if(this.size == "Personal ($10.00)"){
+    this.size = 10;
+  } else if(this.size == "Medium ($15.00)"){
+    this.size = 15;
+  } else if(this.size == "Large ($20.00)") {
+    this.size = 20;
+  } else if(this.size == "X-Large ($25.00)") {
+    this.size = 25;
+  }
+  if(this.number == "2 toppings ($1.00)"){
+    this.number = 1;
+  } else if (this.number == "3 toppings ($2.00)") {
+    this.number = 2;
+  } else {
+    this.number = 0;
+  }
+  return "Your purchase total is: $" + parseInt(this.size + this.number) + ".00";
+}
 //user interface
 $(document).ready(function () {
   function resetFields() {
     $("input#name").val("");
     $("select#size").val("");
     $("select#number").val("");
-    $("select#toppings").val("");
+    $("input#1top").val("");
+    $("input#2top").val("");
+    $("input#3top").val("");
   }
 //purchase button
   $("form#pizza-order").submit(function(event) {
@@ -31,9 +43,10 @@ $(document).ready(function () {
     var nameinput = $("input#name").val();
     var sizeinput = $("select#size").val();
     var numberinput = $("select#number").val();
-    var toppingsinput = $("select#toppings").val();
-
-    var newPizza = new Pizza(nameinput, sizeinput, numberinput, toppingsinput)
+    var top1input = $("input#1top").val();
+    var top2input = $("input#2top").val();
+    var top3input = $("input#3top").val();
+    var newPizza = new Pizza(nameinput, sizeinput, numberinput, top1input, top2input, top3input)
 
     $("h3#ordered").append("<li><span class='pizza'>" + "Thank you, " + newPizza.name + " click here to see your order" + "</span></li>")
 //showorder
@@ -43,13 +56,10 @@ $(document).ready(function () {
       $("#listname").text(newPizza.name);
       $("#listsize").text(newPizza.size);
       $("#listnumber").text(newPizza.number);
-      $("#listtoppings").text(newPizza.toppings);
+      $("ul#topping").text("");
+      $("ul#topping").append("<li>" + newPizza.top1 + ", " + newPizza.top2 + ", " + newPizza.top3 + "</li>")
+      $("#price").text(newPizza.price());
     });
     resetFields();
   });
-
-
-
-
-
 });
